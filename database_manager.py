ec6b5225a252a530a20f6365979017b3d4f6db14
abolfazlyarian <yarianabolfazl@gmail.com>
2023-06-29 18:09:28 +0330

    clean the code

--- database_manager.py
+++ database_manager.py
@@ -1,61 +1,71 @@
 from kubernetes import client, config, watch
 import logging
 import postgres_management as pm
-# Load Kubernetes configuration from default location
+import os
+
 logging.basicConfig(
     level=logging.INFO,
+    format='[%(asctime)s] --> %(message)s \n'
 )
-#config.load_kube_config(config_file="/home/user01/.kube/config")
+logging.info("---------------------- Running ------------------")
+
+# load Kubernetes configuration from default location
 config.load_incluster_config()
 
-# Create a Kubernetes API client
+# create a Kubernetes API client
 api_instance = client.AppsV1Api()
 
-# Define the namespace and resource type to watch
-namespace = 'yarian'
+# define the namespace
+namespace = os.environ.get('namespace', default='default')
 
-# Watch for events in the specified namespace and resource type
+# watch for events in the specified namespace
 stream = watch.Watch()
 
-# Iterate through events
-logging.info("---------------------- Running ------------------")
+# iterate through events
 for event in stream.stream(api_instance.list_namespaced_deployment, namespace):
-    # print("---------------------- Running ------------------")
-    # Check if the event is of type 'ADDED' or 'MODIFIED'
-    # print(f"------------------------ Event {i} ------------------------")
-    # print(event)
-    if event['type'] in ['ADDED']: # = MODIFIED
+    # check the event is the type of 'ADDED' 
+    if event['type'] in ['ADDED']:
+        # extract annotations from metadata
         deployment = event['object']
-        # print(f"------------------------ deployment {i} ------------------------")
-        # print(deployment)
-        # Extract annotations from metadata
         annotations = deployment.metadata.annotations
-        # print(f"------------------------ annotation {i} ------------------------")
-        # print(annotations)
 
-        # Check if the required annotations exist
+        # check the required annotations exist
         db_name = annotations.get('postgresql.db')
         db_user = annotations.get('postgresql.user')
         
         if db_name is not None and db_user is not None:
-            # The annotations exist and are not None
-            # Process the db_name and db_user variables here
             logging.info("db_name: %s", db_name)
             logging.info("db_user: %s", db_user)
-            
-            conn = pm.psql_connection(
-                db_name='postgres',
-                db_user='postgres',
-                db_pass='postgres',
-                host='postgresql-headless.default.svc.cluster.local'
-                )
-        
+
+            # connect to database as superuser
+            try:
+                conn = pm.psql_connection(
+                                            db_name='postgres',
+                                            db_user='postgres',
+                                            db_pass='postgres',
+                                            host='postgresql-headless.default.svc.cluster.local'
+                                         )
+            except Exception as e:
+                logging.error("Failed to connect to the database: %s", str(e))
+
+            # create database
             cursor = conn.cursor()
-            pm.create_database(cursor, db_name=db_name, db_user=db_user, db_pass='12345')
+            db_pass = os.environ.get('DB_PASS', default='12345')
+            pm.create_database(cursor, db_name=db_name, db_user=db_user, db_pass=db_pass)
             conn.close()
-            logging.info("----------- Creating Database for %s --------------", db_name)
             
+            # check the databases is created or not
+            try:
+                conn = pm.psql_connection(
+                                            db_name=db_name,
+                                            db_user=db_user,
+                                            db_pass='12345',
+                                            host='postgresql-headless.default.svc.cluster.local'
+                                         )
+                conn.close()
+                logging.info("The %s database for the %s user has been created successfully", db_name, db_user)
+            except:
+                logging.info("The %s database for the %s user has not been created successfully", db_name, db_user)
+        
         else:
-            # The annotations are missing or None
-            # Handle this case accordingly
             logging.info("Annotations 'postgresql.db' or 'postgresql.user' are missing or None.")
--- postgres_management.py
+++ postgres_management.py
@@ -1,11 +1,14 @@
 import psycopg2
 
 
-def psql_connection(db_name="postgres", db_user='postgres', db_pass='1376920', host='127.0.0.1'):
+def psql_connection(db_name="postgres", db_user='postgres', db_pass='postgres', host='127.0.0.1'):
 
     conn = psycopg2.connect(
-    database=db_name, user=db_user, password=db_pass, host=host, port= '5432'
-    )
+                            database=db_name,
+                            user=db_user, 
+                            password=db_pass, 
+                            host=host, 
+                            port= '5432')
     conn.autocommit = True
 
     return conn
@@ -20,17 +23,6 @@ def create_database(cursor, db_name, db_user, db_pass):
     sql = f"CREATE USER {db_user} WITH ENCRYPTED PASSWORD '{db_pass}';"
     cursor.execute(sql)
 
-    #Grant
+    # Grant
     sql = f"GRANT ALL PRIVILEGES ON DATABASE {db_name} TO {db_user};"
     cursor.execute(sql)
-
-#establishing the connection
-# conn = psql_connection()
-# #Creating a cursor object using the cursor() method
-# cursor = conn.cursor()
-
-# #create a database
-# create_database(cursor, db_name='test3', db_user='abolfazl2', db_pass='123456')
-
-#Closing the connection
-# conn.close()
